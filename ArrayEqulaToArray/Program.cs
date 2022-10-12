51b05afed3b141abf5cfcce25c34908c9b853299
Sakhile <Sakhile@codeplex.co.za>
2022-10-12 05:34:28 +0200

    Add project files.

---
+++ ArrayEqulaToArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayEqulaToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intarray = { 1, 2, 3, 4, 5 };
+
+            int[] intarray1 = new int[5] { 1, 2, 3, 4, 5 };
+
+            //intarray1 = intarray;
+
+            Console.WriteLine(intarray.Equals(intarray1));
+        }
+    }
+}
